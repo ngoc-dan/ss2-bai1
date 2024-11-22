b8c19690282a4b19cebfd4dd00967e4f3350865c
ngoc-dan <ngocdan191026@gmail.com>
2024-11-22 10:29:59 +0700

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>    //khai bao thu vien
+
+int main(){         //khai bao 
+    int x=5;        //khai bao bien va khoi tao
+    float y=3.12;   //khai bao bien y kieu so thuc
+    char z='B';     //khai bao bien z kieu ki tu
+    bool t=true     //khai bao bien t va luu gia tri true
+
+    
+}
